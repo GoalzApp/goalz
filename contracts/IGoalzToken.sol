f584f26b23e06ee25c2dc1bb6dd482d5a11545e7
Mike Ghen <mike@mikeghen.com>
2024-08-29 10:19:44 -0400

    Revert "Aave Integration"

--- contracts/IGoalzToken.sol
+++ contracts/IGoalzToken.sol
@@ -4,5 +4,4 @@ pragma solidity ^0.8.0;
 interface IGoalzToken {
     function mint(address account, uint256 amount) external;
     function burn(address account, uint256 amount) external;
-    function getInterestIndex() external view returns (uint256);
 }
--- test/GoalzToken.js
+++ test/GoalzToken.js
@@ -1,36 +1,17 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-describe("GoalzToken with Aave Integration", function () {
+describe("GoalzToken", function () {
   let GoalzToken;
   let goalzToken;
-  let goalzTokenAddress;
-  let MockAaveToken;
-  let aaveTokenMock;
-  let aaveTokenMockAddress;
-  let ERC20Mock;
-  let usdc;
-  let usdcAddress;
   let deployer;
   let user1;
 
   // Function that loads the GoalzToken fixture, used in each test
   const loadGoalzTokenFixture = async function () {
-    // Deploy mock ERC20 tokens for depositToken and aToken
-    ERC20Mock = await ethers.getContractFactory("ERC20Mock");
-    usdc = await ERC20Mock.deploy("USD Coin", "USDC");
-    usdcAddress = await usdc.getAddress();
-
-    const interestRatePerSecond = ethers.parseEther("3805175038").toString(); // Approx 12% APY
-    MockAaveToken = await ethers.getContractFactory("MockAaveToken");
-    aaveTokenMock = await MockAaveToken.deploy("Aave USD Coin", "aUSDC", interestRatePerSecond);
-    aaveTokenMockAddress = await aaveTokenMock.getAddress();
-    
     GoalzToken = await ethers.getContractFactory("GoalzToken");
-    const _goalzToken = await GoalzToken.deploy("Goalz USD Coin", "glzUSDC", usdcAddress, aaveTokenMockAddress);
-    goalzTokenAddress = await _goalzToken.getAddress();
-
-    return [_goalzToken, usdc, aaveTokenMock];
+    const _goalzToken = await GoalzToken.deploy("Goalz Token", "GLZ");
+    return _goalzToken;
   }
 
   before(async function () {
@@ -39,19 +20,17 @@ describe("GoalzToken with Aave Integration", function () {
 
   describe("Deployment", function () {
     it("should deploy the contract", async function () {
-      [goalzToken, usdc, aaveTokenMock] = await loadGoalzTokenFixture();
+      goalzToken = await loadGoalzTokenFixture();
 
-      expect(await goalzToken.name()).to.equal("Goalz USD Coin");
-      expect(await goalzToken.symbol()).to.equal("glzUSDC");
+      expect(await goalzToken.name()).to.equal("Goalz Token");
+      expect(await goalzToken.symbol()).to.equal("GLZ");
       expect(await goalzToken.owner()).to.equal(deployer.address);
-      expect(await goalzToken.depositToken()).to.equal(usdcAddress);
-      expect(await goalzToken.aToken()).to.equal(aaveTokenMockAddress);
     });
   });
 
   describe("Minting", function () {
     beforeEach(async function () {
-      [goalzToken, usdc, aaveTokenMock] = await loadGoalzTokenFixture();
+      goalzToken = await loadGoalzTokenFixture();
     });
 
     it("should allow the owner to mint tokens", async function () {
@@ -66,7 +45,7 @@ describe("GoalzToken with Aave Integration", function () {
 
   describe("Burning", function () {
     beforeEach(async function () {
-      [goalzToken, usdc, aaveTokenMock] = await loadGoalzTokenFixture();
+      goalzToken = await loadGoalzTokenFixture();
       await goalzToken.mint(user1.address, ethers.parseEther("100"));
     });
 
@@ -80,36 +59,9 @@ describe("GoalzToken with Aave Integration", function () {
     });
   });
 
-  describe("Interest Accrual", function () {
-    beforeEach(async function () {
-      [goalzToken, usdc, aaveTokenMock] = await loadGoalzTokenFixture();
-      await aaveTokenMock.mockBalanceOf(ethers.parseEther("100"));
-      await goalzToken.mint(user1.address, ethers.parseEther("100"));
-    });
-
-    it("should accrue interest over time", async function () {
-      // Check the interest index
-      const startInterestIndex = await goalzToken.getInterestIndex();
-
-      // Increase the time to simulate interest accrual
-      await network.provider.send("evm_increaseTime", [31536000]); // 1 year in seconds
-      await network.provider.send("evm_mine");
-
-      // Set mockBalance to simulate interest
-      await aaveTokenMock.mockBalanceOf(ethers.parseEther("101"));
-
-      // Update the interest
-      await goalzToken.mint(user1.address, 1);
-
-      const endInterestIndex = await goalzToken.getNextInterestIndex();
-
-      expect(endInterestIndex).to.be.gt(startInterestIndex);
-    });
-  });
-
   describe("Transfers", function () {
     beforeEach(async function () {
-      [goalzToken, usdc, aaveTokenMock] = await loadGoalzTokenFixture();
+      goalzToken = await loadGoalzTokenFixture();
       await goalzToken.mint(user1.address, ethers.parseEther("100"));
     });
 
